683f171dfb311ceae455d4b01a4dee18d4015501
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-19 14:36:34 +0530

    Roboshop Components

--- catalogue.sh
+++ catalogue.sh
@@ -13,7 +13,7 @@ print_head "Create Application Directory"
 mkdir /app &>>${log_file}
 
 print_head "Delete Old Content"
-rm -rf/app/* &>>${log_file}
+rm -rf /app/* &>>${log_file}
 
 print_head "Downloading App Content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
@@ -25,13 +25,13 @@ unzip /tmp/catalogue.zip &>>${log_file}
 print_head "Installing NodeJS Dependencies"
 npm install &>>${log_file}
 
-print_head "Copy SystemD Service file"
+print_head "Copy SystemD Service File"
 cp ${code_dir}/configs/catalogue.service /etc/systemd/system/catalogue.service &>>${log_file}
 
 print_head "Reload SystemD"
 systemctl daemon-reload &>>${log_file}
 
-print_head "Enable Catalogue Service"
+print_head "Enable Catalogue Service "
 systemctl enable catalogue &>>${log_file}
 
 print_head "Start Catalogue Service"
@@ -44,5 +44,5 @@ print_head "Install Mongo Client"
 yum install mongodb-org-shell -y &>>${log_file}
 
 print_head "Load Schema"
-mongo --host mongodb.devopswithbhanu.online </app/schema/catalogue.js &>>${log_file}
+mongo --host mongodb.devopsb71.online </app/schema/catalogue.js &>>${log_file}
 
