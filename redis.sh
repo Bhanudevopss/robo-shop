4043e492bf97915fa5f8257836e5b12c67b0fb79
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-20 15:36:01 +0530

    Roboshop Components

--- redis.sh
+++ redis.sh
@@ -12,7 +12,7 @@ print_head "Install Redis"
 yum install redis -y &>>${log_file}
 status_check $?
 
-print_head "Install Redis"
+print_head "Update Redis Listen Address"
 sed -i -e 's/127.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>${log_file}
 status_check $?
 
