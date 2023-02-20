6fe39579676b16eb05753d99d8542576a2ec1f21
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-20 15:27:42 +0530

    Roboshop Components

--- redis.sh
+++ redis.sh
@@ -1,31 +1,14 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
+source common.sh
 
-print_head() {
-  echo -e "\e[36m$1\e[0m"
-}
-
-status_check() {
-  if [ $1 = 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-    echo "Read the log file ${log_file} for more information about error"
-    Exit 1
-  fi
-}
-
-
-print_head "Installing Redis RPM"
+print_head "Installing Redis Repo files"
 yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>{log_file}
 status_check $?
 
-print_head "Enabling Redis 6.2"
+print_head "Enable 6.2 redis repo"
 dnf module enable redis:remi-6.2 -y &>>{log_file}
 status_check $?
 
-print_head "Installing Redis 6.2"
+print_head "Install Redis"
 yum install redis -y &>>{log_file}
 status_check $?
 
@@ -33,8 +16,7 @@ print_head "Enable Redis"
 systemctl enable redis &>>{log_file}
 status_check $?
 
-print_head "Restart Redis"
+print_head "Start Redis"
 systemctl restart redis &>>{log_file}
 status_check $?
-
 
