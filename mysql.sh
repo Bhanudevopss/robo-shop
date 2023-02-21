78ba3fc9687af8e0b9760c69ddd5d7a2894ab68c
Bhanu Pratap Reddy <Bhanu.devopss@gmail.com>
2023-02-21 12:14:57 +0530

    Roboshop Components

---
+++ mysql.sh
@@ -0,0 +1,33 @@
+source common.sh
+
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31mMissing MySQL Root Password Argument\e[0m"
+  exit 1
+fi
+
+
+print_head "Disabling MySQL 8 Version"
+dnf module disable mysql -y &>>${log_file}
+status_check $?
+
+print_head "Copying MySQL repo file"
+cp ${code_dir}/configs/mysql.repo /etc/yum.repos.d/mysql.repo &>>${log_file}
+status_check $?
+
+print_head "Installing MySQL Server"
+yum install mysql-community-server -y &>>${log_file}
+status_check $?
+
+print_head "Enable MySQL Service"
+systemctl enable mysqld &>>${log_file}
+status_check $?
+
+print_head "Start MySQL Service"
+systemctl restart mysqld &>>${log_file}
+status_check $?
+
+print_head "Set Root Password"
+mysql_secure_installation --set-root-pass ${mysql_root_password} &>>${log_file}
+status_check $?
+
